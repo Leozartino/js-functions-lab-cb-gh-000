41c4a037020c8aed555045c8957ea93181f6e37c
Leonardo Oliveira <leozartino@gmail.com>
2019-03-25 11:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // write your code below!
+
 function happyHolidays () {
   return "Happy holidays!";
 }
 
-function happyHolidayTo(name){
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo (holiday, name){
+function happyHolidayTo (holiday, name){
   return `Happy ${holiday}, ${name}`;
 }
 
